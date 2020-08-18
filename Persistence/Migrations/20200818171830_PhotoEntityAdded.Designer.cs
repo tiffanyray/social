8d0018ec490163e0e7b78bd5c4780c2160fb8625
tiffanyray <tiffanyraylee@gmail.com>
2020-08-18 11:34:01 -0600

    Adds photo class to app user Adds migrations for photos

--- Persistence/Migrations/20200818171830_PhotoEntityAdded.Designer.cs
+++ Persistence/Migrations/20200818171830_PhotoEntityAdded.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200818171830_PhotoEntityAdded")]
+    partial class PhotoEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -144,8 +146,6 @@ namespace Persistence.Migrations
                     b.Property<Guid>("ActivityId")
                         .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("DateJoined")
-                        .HasColumnType("TEXT");
 
                     b.Property<bool>("IsHost")
                         .HasColumnType("INTEGER");
