508004d7ef39fd52edc9690d0199dbfb8d14ce14
tiffanyray <tiffanyraylee@gmail.com>
2020-08-18 10:18:37 -0600

    Adds infrastructure for photo accessor. Adds Cloudinary to project for photo storage

---
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,53 @@
+﻿using Application.Interfaces;
+using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor : IPhotoAccessor
+    {
+        private readonly Cloudinary _cloudinary;
+        
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.AppSecret
+                );
+            
+            _cloudinary = new Cloudinary(acc);
+        }
+
+        public PhotoUploadResult AddPhoto(IFormFile file)
+        {
+            var uploadResults = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams
+                    {
+                        File = new FileDescription(file.FileName, stream)
+                    };
+
+                    uploadResults = _cloudinary.Upload(uploadParams);
+                }
+            }
+            return new PhotoUploadResult
+            {
+                PublicId = uploadResults.PublicId,
+                Url = uploadResults.SecureUrl.AbsoluteUri
+            };
+        }
+
+        public string DeletePhoto(string publicId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
